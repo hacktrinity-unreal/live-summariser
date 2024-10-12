1ad602cca48d6420a59314b5283292fcb9cc0226
Ignacy <ignacysus@gmail.com>
2024-10-12 14:57:33 +0100

    Template for AIExpert component with CSS (#7)
    lgtm

--- live-sum-app/src/App.js
+++ live-sum-app/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './home.js';
-import Page_1 from './page_1.js';
+// import Page_1 from './page_1.js';
+import AIExpert from './page_1';
 
 function App() {
   return (
     <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/page_1" element={<Page_1 />} />
+          {/* <Route path="/page_1" element={<Page_1 />} /> */}
+          <Route path="/ai_expert" element ={<AIExpert/>}/> 
         </Routes>
     </BrowserRouter>
   );
---
+++ live-sum-app/src/page_1.js
@@ -0,0 +1,35 @@
+import React,{ useState }  from 'react';
+import './index.css'; // Import the CSS file
+// function PageComponent(){
+//     <Key></Key>
+//     <AiExpert></AiExpert>
+// }
+function AIExpert(){
+    const [showOpinionAI, setShowOpinionAI] = useState(false)
+    const handleButtonClick = () => {
+        setShowOpinionAI(true); // Toggle the state
+    };
+    return (
+        <div className="ai-expert-container">
+            <div className="opinion-container"> 
+                <h1>AI Expert</h1>
+                {showOpinionAI && <OpinionAI />}
+            </div>
+            
+            <button onClick={handleButtonClick} className='opinion-button'>
+            Give AI opinion
+            </button>
+        </div>
+    )
+}
+function OpinionAI(){
+    return (
+        <div className="opinion-ai-box">
+            
+            <p>This should be the opinion of the AI expert</p>
+        </div>
+    )
+}
+
+
+export default AIExpert;
