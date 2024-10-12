a6efd9e3b90519774517c7a29705610ca9a0d6b2
david forbes <davidforbes321@gmail.com>
2024-10-12 19:30:44 +0100

    x

---
+++ bet/betserver.py
@@ -0,0 +1,25 @@
+from aiohttp import web
+import json
+
+async def handle_add_bet(request):
+    request.Response
+    
+    return web.Response(status=200)
+
+async def handle_get_returns(request):
+    response = json.dumps({
+        "guilty":1.5,
+        "not_guilty":1.4
+
+    })
+    return web.Response(body=response)
+
+
+
+app = web.Application()
+#position is either "guilty" or "not_guilty"
+app.add_routes([web.post('/bet/{amount}/{position}', handle_add_bet),
+                web.get('/get_returns', handle_get_returns)])
+
+if __name__ == '__main__':
+    web.run_app(app)
