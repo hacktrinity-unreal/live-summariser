46cee6502a1a5f3acba7816591830a5621991363
david forbes <davidforbes321@gmail.com>
2024-10-13 11:32:50 +0100

    add unit test

--- bet/betserver.py
+++ bet/betserver.py
@@ -1,50 +1,47 @@
 from aiohttp import web
 import json
 from bet import Bet
+
 PATH = "bets.json"
 
 
-CORS_HEADERS = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Headers": "*"
-}
+CORS_HEADERS = {"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": "*"}
 
 
 async def handle_add_bet(request):
     data = await request.json()
+    data = json.loads(data)
     bet = Bet(PATH)
-    
-    print(data)
 
-    stake = data['stake'] if 'stake' in data else 1
-    position = data['position'] if 'position' in data else 'guilty'
-    bet.add(stake,position)
+    print("data")
+
+    stake = data["stake"] if "stake" in data else 1
+    position = data["position"] if "position" in data else "guilty"
+    bet.add_bet(stake, position)
     bet.save_to(PATH)
 
-    
-    response_data = {
-            'status': 'success',
-            'received': data
-        }
-    return web.json_response(body=response_data, headers=CORS_HEADERS)
+    response_data = {"status": "success", "received": data}
+    return web.json_response(body=json.dumps(response_data), headers=CORS_HEADERS)
+
 
 async def handle_get_returns(request):
     bet = Bet(PATH)
 
-    prob2odds = lambda x:x/(x-1)
-    response = json.dumps({
-        "guilty":prob2odds(bet.get_odds()[0]),
-        "not_guilty":prob2odds(bet.get_odds()[1])
-
-    })
+    prob2odds = lambda x: x / (x - 1)
+    response = json.dumps(
+        {
+            "guilty": prob2odds(bet.get_odds()[0]),
+            "not_guilty": prob2odds(bet.get_odds()[1]),
+        }
+    )
     return web.json_response(body=response, headers=CORS_HEADERS)
 
 
-
 app = web.Application()
-#position is either "guilty" or "not_guilty"
-app.add_routes([web.get('/bet', handle_add_bet),
-                web.get('/get_returns', handle_get_returns)])
+# position is either "guilty" or "not_guilty"
+app.add_routes(
+    [web.post("/bet", handle_add_bet), web.get("/get_returns", handle_get_returns)]
+)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     web.run_app(app)
--- bet/bettests.py
+++ bet/bettests.py
@@ -1,26 +1,53 @@
 from bet import Bet
 import random
 import unittest
+import os
+import subprocess
+import requests
+import time
+import json
+
 
 class TestBet(unittest.TestCase):
 
     def test_runs(self):
         bet = Bet()
-        
-        for x in [(["guilty","not_guilty"][random.randint(0,1)],random.randint(1,100)) for _ in range(100)]:
-            bet.add_bet(x[1],x[0])
-        self.assertEqual(sum(bet.get_odds()),1)
+
+        for x in [
+            (["guilty", "not_guilty"][random.randint(0, 1)], random.randint(1, 100))
+            for _ in range(100)
+        ]:
+            bet.add_bet(x[1], x[0])
+        self.assertAlmostEqual(sum(bet.get_odds()), 1)
 
     def test_save(self):
         bet = Bet()
-        
-        for x in [(["guilty","not_guilty"][random.randint(0,1)],random.randint(1,100)) for _ in range(100)]:
-            bet.add_bet(x[1],x[0])
-        
+
+        for x in [
+            (["guilty", "not_guilty"][random.randint(0, 1)], random.randint(1, 100))
+            for _ in range(100)
+        ]:
+            bet.add_bet(x[1], x[0])
+
         bet.save_to("bets.json")
 
         bet2 = Bet(path="bets.json")
-        self.assertEqual(bet.bets,bet2.bets)
+        self.assertEqual(bet.bets, bet2.bets)
+
+    def test_post(self):
+        server = subprocess.Popen(["python", "betserver.py"])
+
+        content = {
+            "position": "guilty",
+            "stake": 50,
+        }
+        time.sleep(2)
+        r = requests.post("http://0.0.0.0:8080/bet", json=json.dumps(content))
+
+        print(r)
+        server.terminate()
+        assert r.status_code == 200
+
 
 if __name__ == "__main__":
     unittest.main()
